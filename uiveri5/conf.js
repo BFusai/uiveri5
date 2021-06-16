852cd42aa9f91d0964b8b118962bb22f611ae61a
Beatrice Fusai <beatrice.fusai@accenture.com>
2021-06-16 12:34:11 +0000

    add node modules

--- uiveri5/conf.js
+++ uiveri5/conf.js
@@ -1,5 +1,5 @@
 //In this file, you can define, for example, the browser and reporter that are used, the base URL, and the credentials for your login dialog.
 exports.config = {
   profile: "integration",
-  baseUrl: "https://sapui5.hana.ondemand.com/test-resources/sap/m/demokit/cart/webapp/index.html"
+  baseUrl: "https://sapui5.hana.ondemand.com/test-resources/sap/m/demokit/cart/webapp/index.html",
 };
